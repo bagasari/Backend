069944ea4db1c2c2f506636133708ebd033f3d64
eckrin <bsh51515@naver.com>
2023-03-17 21:17:27 +0900

    add: init project

---
+++ src/main/java/com/bagasari/sacbagaji/SacbagajiApplication.java
@@ -0,0 +1,13 @@
+package com.bagasari.sacbagaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacbagajiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacbagajiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bagasari/sacbagaji/SacbagajiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagasari.sacbagaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacbagajiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
